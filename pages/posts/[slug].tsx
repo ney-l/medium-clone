c124e32ef3c08300e0d231b425376de38849fa7e
Ney Lanjewar <neylanjewar@gmail.com>
2022-02-23 21:30:36 +0530

    fetches post data on the server

---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,73 @@
+import { Header } from 'components/layout/Header'
+import { GetStaticProps } from 'next'
+import { sanityClient } from 'sanity'
+import { Post } from 'typings'
+
+interface IProps {
+  post: Post
+}
+
+export default function PostPage({ post }: IProps) {
+  console.log(post)
+  return (
+    <main>
+      <Header />
+    </main>
+  )
+}
+
+export const getStaticPaths = async () => {
+  const query = `
+    *[_type == "post"] {
+      _id,
+      slug {
+        current
+      }
+    }
+  `
+
+  const posts = await sanityClient.fetch(query)
+  const paths = posts.map((post: Post) => ({
+    params: {
+      slug: post.slug.current,
+    },
+  }))
+
+  return {
+    paths,
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const query = `
+    *[_type == "post" && slug.current == $slug][0] {
+      _id,
+      _createdAt,
+      title,
+      author-> {
+        name,
+        image
+      },
+      description,
+      mainImage,
+      slug,
+      body
+    }
+  `
+
+  const post = await sanityClient.fetch(query, {
+    slug: params?.slug,
+  })
+
+  if (!post) {
+    return {
+      notFound: true, // nextjs returns a 404 page
+    }
+  }
+
+  return {
+    props: { post },
+    revalidate: 60 /* updates catch after 60 seconds */,
+  }
+}
