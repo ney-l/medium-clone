8a776592c03d68b8d1b823e1fe42842ae566717d
Ney Lanjewar <neylanjewar@gmail.com>
2022-02-24 14:47:39 +0530

    adds component to render post content

---
+++ components/posts/Post.tsx
@@ -0,0 +1,57 @@
+import PortableText from 'react-portable-text'
+import { urlFor } from 'sanity'
+import { IPostProps } from 'typings'
+
+export function Post({ post }: IPostProps) {
+  return (
+    <>
+      <img
+        className="h-40 w-full object-cover"
+        src={urlFor(post.mainImage).url()}
+        alt={post.title}
+      />
+      <article className="mx-auto max-w-3xl p-5">
+        <h1 className="mt-10 mb-3 text-3xl">{post.title}</h1>
+        <h2 className="text-xl font-light">{post.description}</h2>
+
+        <div className="flex items-center space-x-2">
+          <img
+            className="h-10 w-10 rounded-full"
+            src={urlFor(post.author.image).url()}
+            alt={post.author.name}
+          />
+          <p className="text-sm font-extralight">
+            Blog post by{' '}
+            <span className="text-green-600">{post.author.name}</span> -
+            Published at {new Date(post._createdAt).toLocaleString()}
+          </p>
+        </div>
+
+        <div>
+          <PortableText
+            className=""
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
+            content={post.body}
+            serializers={{
+              h1: (props: any) => (
+                <h1 className="my-5 text-2xl font-bold" {...props} />
+              ),
+              h2: (props: any) => (
+                <h1 className="my-5 text-xl font-bold" {...props} />
+              ),
+              li: ({ children }: any) => (
+                <li className="ml-4 list-disc">{children} </li>
+              ),
+              link: ({ href, children }: any) => (
+                <a href={href} className="text-blue-500 hover:underline">
+                  {children}{' '}
+                </a>
+              ),
+            }}
+          />
+        </div>
+      </article>
+    </>
+  )
+}
