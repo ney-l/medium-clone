fa6ee71eab5932e9e9c03f5d9748ef8f1b9bfd22
Ney Lanjewar <neylanjewar@gmail.com>
2022-02-25 15:12:08 +0530

    adds a dumb component for comments

---
+++ components/comments/Comments.tsx
@@ -0,0 +1,16 @@
+import { IComment } from 'typings'
+import { Comment } from './Comment'
+
+interface ICommentsProps {
+  comments: IComment[]
+}
+
+export function Comments({ comments }: ICommentsProps) {
+  return (
+    <div>
+      {comments.map((comment) => (
+        <Comment key={comment._id} comment={comment} />
+      ))}
+    </div>
+  )
+}
