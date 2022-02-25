7d1354b89832f07f830f53d6258ddc3edddfebc3
Ney Lanjewar <neylanjewar@gmail.com>
2022-02-25 21:07:43 +0530

    adds page title

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -6,6 +6,7 @@ import { Post } from '@/components/posts'
 import { useCreateComment } from '@/hooks/comments.hooks'
 import { AddComment, Comments } from '@/components/comments'
 import { getPost, getPostSlugs } from '@/queries/posts'
+import Head from 'next/head'
 
 export default function PostPage({ post }: IPostProps) {
   const { isSuccess, isLoading, error, onSubmit } = useCreateComment(post._id)
@@ -16,32 +17,37 @@ export default function PostPage({ post }: IPostProps) {
   }
 
   return (
-    <main>
-      <Header />
-      <Post post={post} />
-      <hr className="my-5 mx-auto max-w-lg border border-yellow-500" />
-      {isSuccess ? (
-        <div className="my-10 mx-auto flex max-w-2xl flex-col bg-yellow-500 py-10 text-center text-white">
-          <h3 className="font-bond text-3xl">
-            Thank you for submitting your comment!
-          </h3>
-          <p>Once it has been approved, it will appear below!</p>
+    <>
+      <Head>
+        <title>{post.title} | Medium Clone</title>
+      </Head>
+      <main>
+        <Header />
+        <Post post={post} />
+        <hr className="my-5 mx-auto max-w-lg border border-yellow-500" />
+        {isSuccess ? (
+          <div className="my-10 mx-auto flex max-w-2xl flex-col bg-yellow-500 py-10 text-center text-white">
+            <h3 className="font-bond text-3xl">
+              Thank you for submitting your comment!
+            </h3>
+            <p>Once it has been approved, it will appear below!</p>
+          </div>
+        ) : (
+          <AddComment
+            onSubmit={handleSubmit}
+            error={error}
+            isLoading={isLoading}
+          />
+        )}
+
+        <div className="my-10 mx-auto flex max-w-2xl flex-col space-y-2 p-10 shadow shadow-yellow-500">
+          <h3 className="text-4xl">Comments</h3>
+          <hr className="pb-2" />
+
+          <Comments comments={post.comments} />
         </div>
-      ) : (
-        <AddComment
-          onSubmit={handleSubmit}
-          error={error}
-          isLoading={isLoading}
-        />
-      )}
-
-      <div className="my-10 mx-auto flex max-w-2xl flex-col space-y-2 p-10 shadow shadow-yellow-500">
-        <h3 className="text-4xl">Comments</h3>
-        <hr className="pb-2" />
-
-        <Comments comments={post.comments} />
-      </div>
-    </main>
+      </main>
+    </>
   )
 }
 
