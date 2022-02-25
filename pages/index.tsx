c73cea1a29aa2d201f7aadf453b986c65e2f0198
Ney Lanjewar <neylanjewar@gmail.com>
2022-02-25 15:16:50 +0530

    abstracts Banner to its own component

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { sanityClient, urlFor } from 'sanity'
 import { Post } from 'typings'
 import { PostCard } from 'components/posts/PostCard'
 import { Footer } from 'components/layout/Footer'
+import { Banner } from 'components/layout/Banner'
 
 interface Props {
   posts: [Post]
@@ -20,27 +21,7 @@ export default function Home({ posts }: Props) {
 
       <div className="">
         <Header />
-        <div className="flex items-center justify-between border-y border-black bg-blue-200 py-10 lg:py-0">
-          <div className="space-y-5 px-10">
-            <h1 className="max-w-xl font-serif text-6xl">
-              <span className="underline decoration-black decoration-4">
-                Medium
-              </span>{' '}
-              is a place to write, read, and connect
-            </h1>
-            <h2 className="font-sans">
-              It's easy and free to post your thinking on any topic and connect
-              with millions of readers.
-            </h2>
-            <Link href="signup">
-              <a>Start Writing</a>
-            </Link>
-          </div>
-          <img
-            className="hidden h-32 pt-3 md:inline-flex md:w-1/3 lg:h-full"
-            src="/images/banner.png"
-          />
-        </div>
+        <Banner />
 
         <div className="grid grid-cols-1 gap-3 bg-white p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3">
           {posts.map((post) => (
