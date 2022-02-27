dc7d7ae07b800da0ceba22594b553fb8bba81fb2
Ney Lanjewar <neylanjewar@gmail.com>
2022-02-27 15:13:37 +0530

    fixes attribute name

--- src/components/icons/CloseIcon.tsx
+++ src/components/icons/CloseIcon.tsx
@@ -3,7 +3,7 @@ export function CloseIcon() {
     <svg width="29" height="29">
       <path
         d="M20.13 8.11l-5.61 5.61-5.6-5.61-.81.8 5.61 5.61-5.61 5.61.8.8 5.61-5.6 5.61 5.6.8-.8-5.6-5.6 5.6-5.62"
-        fill-rule="evenodd"
+        fillRule="evenodd"
       ></path>
     </svg>
   )
